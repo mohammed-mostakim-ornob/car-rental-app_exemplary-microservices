aab1bbd265129c7bc65fe1ddb55512a86fe22d86
Mohammed Mostakim Ornob <mohammed.ornob@ipks.de>
2021-10-15 20:08:57 +0200

    Add MySQL support

--- car-service/src/main/resources/data.sql
+++ car-service/src/main/resources/data.sql
@@ -1,13 +1,20 @@
-insert into manufacturer
-    (name)
-values
+INSERT INTO manufacturer
+(
+    name
+)
+VALUES
     ('Audi'),
     ('BMW'),
     ('Mercedes');
 
-insert into car
-    (code, manufacturer_name, model, rent_per_kilo)
-values
+INSERT INTO car
+(
+    code,
+    manufacturer_name,
+    model,
+    rent_per_kilo
+)
+VALUES
     ('2021_001', 'Audi', 'A3', 0.1),
     ('2021_002', 'BMW', '3 Series', 0.1),
     ('2021_003', 'Mercedes', 'C Class', 0.1);
