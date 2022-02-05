479cfff773221d544f8f9bccaef68cdc8286785b
Mohammed Mostakim Ornob <mostakim.ornob@gmail.com>
2022-02-06 00:58:55 +0100

    Add architecture extraction aggregation service skeleton

---
+++ architecture-extraction/aggregation-service/src/test/java/de/uniba/dsg/microservice/architecture/extraction/aggregationservice/AggregationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.uniba.dsg.microservice.architecture.extraction.aggregationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
